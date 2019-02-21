ad3c3ade025f0c367b7a0bbb68ee6577471affaf
Tomasz Wieckiewicz <tomasz.wieckiewicz@sunzinet.com>
2019-02-21 21:48:15 +0100

    Jenkinsfile

--- build/clearcache.sh
+++ build/clearcache.sh
@@ -2,4 +2,4 @@
 
 BUILD_TIME=$(($CURRENT_BUILD / 1000))
 
-ssh $CONNECTION "$DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/var/cache/clear_cache.sh"
+ssh $CONNECTION "$DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/bin/console sw:cache:clear"
