6416a741398d83a900a985f12263e04d885f28b8
Tomasz Wieckiewicz <tomasz.wieckiewicz@sunzinet.com>
2019-02-22 10:03:27 +0100

    build change

--- build/clearcache.sh
+++ build/clearcache.sh
@@ -2,4 +2,5 @@
 
 BUILD_TIME=$(($CURRENT_BUILD / 1000))
 
-ssh $CONNECTION "$DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/bin/console sw:cache:clear"
+#ssh $CONNECTION "php $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/bin/console sw:cache:clear"
+ssh $CONNECTION "rm $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/web/cache/*.*"
