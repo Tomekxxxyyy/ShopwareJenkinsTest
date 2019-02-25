29c9c1fc06f8c9a48420815e0b830ab02b622878
Tomasz Wieckiewicz <tomasz.wieckiewicz@sunzinet.com>
2019-02-25 12:58:26 +0100

    Jenkinsfile

--- build/backup.sh
+++ build/backup.sh
@@ -2,6 +2,6 @@
 
 BUILD_TIME=$(($CURRENT_BUILD / 1000))
 
-#ssh $CONNECTION "mysqldump -u root -proot shopware > $MYSQLDUMP_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT).sql"
-ssh $CONNECTION "cp  --preserve=mode,ownership -R $DEPLOYMENT_PATH/$(ssh $CONNECTION ls -t $DEPLOYMENT_PATH | head -n 1) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
-#ssh $CONNECTION "cp  --preserve=mode,ownership -R $DEPLOYMENT_PATH/$(ssh $CONNECTION readlink $SYMLINK_PATH) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
+ssh $CONNECTION "mysqldump -u root -proot shopware > $MYSQLDUMP_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT).sql"
+#ssh $CONNECTION "cp  --preserve=mode,ownership -R $DEPLOYMENT_PATH/$(ssh $CONNECTION ls -t $DEPLOYMENT_PATH | head -n 1) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
+ssh $CONNECTION "cp  --preserve=mode,ownership -R $DEPLOYMENT_PATH/$(ssh $CONNECTION readlink $SYMLINK_PATH) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
