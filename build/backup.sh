95d6b5b75f6d7fd85fb28b5d4c09dc0cc49b245d
Tomasz Wieckiewicz <tomasz.wieckiewicz@sunzinet.com>
2019-04-09 12:11:06 +0200

    build

--- build/backup.sh
+++ build/backup.sh
@@ -2,6 +2,5 @@
 
 BUILD_TIME=$(($CURRENT_BUILD / 1000))
 
-ssh $CONNECTION "mysqldump -u root -proot shopware > $MYSQLDUMP_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT).sql"
-#ssh $CONNECTION "cp  --preserve=mode,ownership -R $DEPLOYMENT_PATH/$(ssh $CONNECTION ls -t $DEPLOYMENT_PATH | head -n 1) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
-ssh $CONNECTION "cp  --preserve=mode,ownership -R $DEPLOYMENT_PATH/$(ssh $CONNECTION readlink $SYMLINK_PATH) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
+#ssh $CONNECTION "mysqldump -u root -proot shopware > $MYSQLDUMP_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT).sql"
+ssh $CONNECTION "cp - a $DEPLOYMENT_PATH/$(ssh $CONNECTION readlink $SYMLINK_PATH) $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
