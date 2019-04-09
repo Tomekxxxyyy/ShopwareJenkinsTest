66f56c4bbdfe1fd0eda77c51a0d78fb01d75c0fa
Tomasz Wieckiewicz <tomasz.wieckiewicz@sunzinet.com>
2019-04-09 22:40:05 +0200

    delete added

--- build/deletereleases.sh
+++ build/deletereleases.sh
@@ -2,5 +2,5 @@
 
 BUILD_TIME=$(($CURRENT_BUILD / 1000))
 
-ssh $CONNECTION "ls $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT) | head -n -5 | xargs rm -R"
+ssh $CONNECTION "ls $DEPLOYMENT_PATH head -n -5 | xargs rm -R"
 
