756667c3d22b5e5705f295be67d506dd90c4602b
Tomasz Wieckiewicz <tomasz.wieckiewicz@sunzinet.com>
2019-04-10 11:15:07 +0200

    build

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,8 +2,8 @@
 
 BUILD_TIME=$(($CURRENT_BUILD / 1000))
 
-tar -czvf $BUILD_TIME.tar.gz --exclude='.git' ./
-scp $BUILD_TIME.tar.gz $CONNECTION:$DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)
-ssh $CONNECTION "tar -xzvf $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/$BUILD_TIME.tar.gz -C $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
-ssh $CONNECTION "rm -R $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/$BUILD_TIME.tar.gz"
-rm -R $BUILD_TIME.tar.gz
+#tar -czvf $BUILD_TIME.tar.gz --exclude='.git' ./
+#scp $BUILD_TIME.tar.gz $CONNECTION:$DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)
+#ssh $CONNECTION "tar -xzvf $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/$BUILD_TIME.tar.gz -C $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)"
+#ssh $CONNECTION "rm -R $DEPLOYMENT_PATH/$(ssh $CONNECTION date -d  @$BUILD_TIME +$FORMAT)/$BUILD_TIME.tar.gz"
+#rm -R $BUILD_TIME.tar.gz
